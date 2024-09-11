762670e7974110530612b85069fcf0379714123a
NataliVod <n3824@yandex.ru>
2024-09-11 15:37:35 +0300

    Add resources

--- app/src/main/java/com/ig81/igshop/ui/theme/Color.kt
+++ app/src/main/java/com/ig81/igshop/ui/theme/Color.kt
@@ -9,3 +9,12 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val PrimaryColor = Color(0xFF4FDAD7)          // #4FDAD7
+val OnPrimaryColor = Color(0xFFABB4E7)         // #ABB4E7
+val SecondaryColor = Color(0xFF70E3D6)        // #70E3D6
+val OnSecondaryColor = Color(0xFFABB4E7)      // #ABB4E7
+val SurfaceColor = Color(0xFFFFFFFF)          // #FFFFFF
+val OnSurfaceColor = Color(0xFFE5FDFB)        // #E5FDFB
+val BackgroundColor = Color(0xFF2E4552)       // #2E4552
+val OnBackgroundColor = Color(0xFFCACACA)     // #CACACA
---
+++ app/src/main/java/com/ig81/igshop/ui/theme/Shape.kt
@@ -0,0 +1,12 @@
+package com.ig81.igshop.ui.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.ui.unit.dp
+
+
+object JetIgshopShape {
+    val small = RoundedCornerShape(8.dp)
+    val medium = RoundedCornerShape(16.dp)
+    val large = RoundedCornerShape(16.dp)
+    val dialog = RoundedCornerShape(24.dp)
+}
